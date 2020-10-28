3349e719196ac10ca7e1cf019b85184d204f4f03
xfaramir <xfaramir@gmail.com>
2020-10-28 13:39:33 -0500

    githubActionsTest

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-var dir = __dirname + '/newman/'; // your directory
+var dir = __dirname + '/newman/'; // your directory test
 console.log(dir);
 
 var files = fs
